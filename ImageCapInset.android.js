47fcff61da966b6386b7a94377eaba503db7834d
Marq Roldan <hcmlroldan@gmail.com>
2020-01-26 01:05:46 +0800

    Fixed issue with null values and added styling props for image view to offset cap insets

--- ImageCapInset.android.js
+++ ImageCapInset.android.js
@@ -3,14 +3,26 @@ import PropTypes from 'prop-types';
 import {
     View,
     Image,
+    PixelRatio,
     requireNativeComponent,
 } from 'react-native';
 
 import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
 
+const pxToDP = (n = 0) => PixelRatio.roundToNearestPixel(n / (PixelRatio.get()));
+
 class ImageCapInset extends React.Component {
     render() {
-        const {children, source, capInsets, ...rest} = this.props;
+        const {children, source, capInsets, capStyle = {}, ...rest} = this.props;
+        const { top = 0, left = 0, right = 0, bottom = 0, ...restCapStyle } = capStyle;
+        const FinCapStyle = {
+            opacity: rest.style && rest.style.opacity ? rest.style.opacity: 1,
+            top: pxToDP(top),
+            bottom: pxToDP(bottom),
+            right: pxToDP(right),
+            left: pxToDP(left),
+        };
+
         const normalizedSource = resolveAssetSource(source);
         // noinspection JSUnresolvedVariable
         return (
@@ -19,7 +31,16 @@ class ImageCapInset extends React.Component {
                     capInsets={capInsets}
                     resizeMode={'stretch'}
                     source={normalizedSource}
-                    style={{position: 'absolute', top: 0, left: 0, bottom: 0, right: 0}} />
+                    style={[{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        bottom: 0,
+                        right: 0
+                    },
+                    FinCapStyle,
+                    restCapStyle,
+                    ]} />
                 {children}
             </View>
         );
--- README.md
+++ README.md
@@ -9,11 +9,17 @@ import ImageCapInset from 'rn-imagecapinsets';
 import BubbleImage from './images/bubble.png';
 
 <ImageCapInset source={BubbleImage}
+               capStyle={{
+                 top: -14,
+                 bottom: -26,
+                 left: -16,
+                 right: -24,
+               }}
                capInsets={{ top: 8, right: 8, bottom: 8, left: 8 }} />
 ```
 
 ## Credits
 
 Continued development under RN 0.60
-[react-native-image-capinsets](https://github.com/GeniusTony/react-native-imagecapinsets)
+[react-native-image-capinsets](https://github.com/marqroldan/react-native-imagecapinsets)
 package.
